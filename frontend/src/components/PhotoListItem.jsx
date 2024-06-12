b08a744b27da4f4134a65f47a6c6138cdbfd1b86
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-12 12:00:16 -0400

    add return to PhotoListItem to render photo and photographer info

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,28 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
-const PhotoListItem = () => {
-  /* Insert React */
+
+const PhotoListItem = (props) => {
+  const {imageSource, profile, username, location} = props.sampleData
+  return (
+    <div>
+      <img src={imageSource} />
+      <img src={profile}/>
+      <h4>{username}</h4>
+      <h5>{location.city} {location.country}</h5>
+    </div>
+  );
 };
 
 export default PhotoListItem;
