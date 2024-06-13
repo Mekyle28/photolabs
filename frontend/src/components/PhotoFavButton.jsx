cb942bc37c3c413e1b4d92321bf98e73909b8bdb
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-12 21:13:42 -0400

    simplify the on click handler to toggle between true and false

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -5,12 +5,12 @@ import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
   const [fav, setFav] = useState(false);
-
+ 
+  // toggles between true and false prev is not whatever prev was. 
   const clickHandler = (() => {
-    console.log("button clicked");
-    console.log("fav", fav);
-    setFav((prev) => prev === false ? true : false)
+      setFav(prev => !prev)
   })
+
   return (
     <div onClick={clickHandler} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
