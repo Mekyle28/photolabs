5e27d30306b90bc7e5576e66d4f544c770e49420
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-13 23:49:02 -0400

    import modal, add state and conditionally render if state is true

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeRoute from 'routes/HomeRoute';
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
 
 
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-
+ const [showModal, setModal] = useState(false);
   //const photos = [...Array(3)].map((_, i) => <PhotoListItem key={i} sampleData={sampleDataForPhotoListItem}/> );
-
+  const modalToggle = (() => {
+    setModal(prev => !prev)
+  });
 
   return (
     <div>
-      <HomeRoute photos={photos} topics={topics}/>
+      <HomeRoute photos={photos} topics={topics} modalToggle={modalToggle}/>
+      {showModal && <PhotoDetailsModal />}
     </div>
   );
 };
