84e41989a3bc8bd93f161581e283ccb0a2a86489
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-13 20:57:27 -0400

    call the function FavPhotoExist and pass as props to the FavBadge

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,12 +5,12 @@ import '../styles/TopNavigationBar.scss'
 import FavBadge from './FavBadge';
 
 const TopNavigation = (props) => {
-  const {photos, fav} = props;
+  const {photos, fav, isFavPhotoExist} = props;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
       <TopicList topics={props.topics} />
-      <FavBadge photos={photos} fav={fav}/> 
+      <FavBadge photos={photos} fav={fav} isFavPhotoExist={isFavPhotoExist}/> 
     </div>
   )
 }
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
+import { FavPhotoExist } from 'helpers/favPhoto';
 
 const HomeRoute = (props) => {
   const {photos, topics} = props;
@@ -13,10 +14,12 @@ const HomeRoute = (props) => {
       {...prev, [photoId]: !prev[photoId]}
     ))
 };
+
+  const isFavPhotoExist = FavPhotoExist(fav);
   
   return (
     <div className="home-route">
-      <TopNavigation topics={topics} photos={photos} fav={fav} />
+      <TopNavigation topics={topics} photos={photos} fav={fav} isFavPhotoExist={isFavPhotoExist}/>
       <PhotoList photos={photos} fav={fav} clickHandler={clickHandler}/>
     </div>
   );
