5bdb3fbf34e3aad98c193968643d9575839706f1
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-15 00:44:29 -0400

    add similar pictures to the modal and add css styling

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,7 +4,7 @@ import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
 import PhotoFavButton from 'components/PhotoFavButton';
-
+import PhotoListItem from 'components/PhotoListItem';
 
 
 const PhotoDetailsModal = (props) => {
@@ -17,26 +17,28 @@ const PhotoDetailsModal = (props) => {
   const favClickHandler = (() => clickHandler(showModal.id))
 
 
+  
+  // input array of photo or photos
 
-  // // input array of photo or photos
+    const modalSimilarPhotos = Object.values(modalMainImg.
+    similar_photos).map((photo) => {
+      return ( 
+        <PhotoListItem 
+          key={photo.id}
+          id={photo.id}
+          imageSource={photo.urls.regular}
+          profile={photo.user.profile}
+          username={photo.user.name}
+          city={photo.location.city}
+          country={photo.location.country}
+          fav={fav}
+          clickHandler={() => clickHandler(photo.id)}
+          modalToggle={() => console.log("onClick disabled")}
+        />
+      );
+    });
 
-  //   const modalMainPhoto = modalMainPhotoArr.map((photo) => {
-  //     return (
-  //       <PhotoListItem 
-  //         key={photo.id}
-  //         id={photo.id}
-  //         imageSource={photo.urls.regular}
-  //         profile={photo.user.profile}
-  //         username={photo.user.name}
-  //         city={photo.location.city}
-  //         country={photo.location.country}
-  //         fav={fav}
-  //         clickHandler={() => clickHandler(photo.id)}
-  //         modalToggle={""}
-  //       />
-  //     );
-  //   });
-
+    console.log(modalSimilarPhotos);
   return (
     <div className="photo-details-modal">
       <button onClick={closeModal} className="photo-details-modal__close-button">
@@ -44,17 +46,23 @@ const PhotoDetailsModal = (props) => {
       </button>
       
       <div className="photo-details-modal__images">
-      <span onClick={favClickHandler} ><PhotoFavButton fav={fav[modalMainImg.id]} /></span>
-      <img  className="photo-details-modal__image" src={modalMainImg.urls.full} />
-    
-      <div className="photo-details-modal__photographer-details">
-        <img className="photo-details-modal__photographer-profile" src={modalMainImg.user.profile}/>
-        <div className="photo-details-modal__photographer-info">
-          <p className="" >{modalMainImg.user.name}</p>
-          <span className="photo-details-modal__photographer-location">{modalMainImg.location.city}, {modalMainImg.location.country}</span>
+        <span onClick={favClickHandler} ><PhotoFavButton fav={fav[modalMainImg.id]} /></span>
+        <img  className="photo-details-modal__image" src={modalMainImg.urls.regular} />
+      
+        <div className="photo-details-modal__photographer-details">
+          <img className="photo-details-modal__photographer-profile" src={modalMainImg.user.profile}/>
+          <div className="photo-details-modal__photographer-info">
+            <p className="" >{modalMainImg.user.name}</p>
+            <span className="photo-details-modal__photographer-location">{modalMainImg.location.city}, {modalMainImg.location.country}</span>
+          </div>
         </div>
+        <header className='photo-details-modal__header'>Similar Photos</header>  
+
       </div>
-    </div>
+      
+      
+        <section className='photo-details-modal__images'>{modalSimilarPhotos}</section>
+      
     </div>
   )
 };
