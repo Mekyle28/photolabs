dd96b730ff6cd596e5a1dc180cf13bd1bf6d813f
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-06-15 12:41:15 -0400

    format and clean up code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,37 @@
-import React, { useState } from 'react';
-import HomeRoute from 'routes/HomeRoute';
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
-import PhotoDetailsModal from 'routes/PhotoDetailsModal';
-import PhotoListItem from 'components/PhotoListItem';
-import { useApplicationData } from 'hooks/useApplicationData';
-
+import React, { useState } from "react";
+import HomeRoute from "routes/HomeRoute";
+import photos from "mocks/photos";
+import topics from "mocks/topics";
+import PhotoDetailsModal from "routes/PhotoDetailsModal";
+import { useApplicationData } from "hooks/useApplicationData";
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-
-//   const [fav , setFav] = useState({});
-  
-//   const clickHandler = (photoId) => {
-//     setFav(prev => (
-//       {...prev, [photoId]: !prev[photoId]}
-//     ))
-//   };
-  
-//  const [showModal, setModal] = useState({display: false, id: ""});
-  
-  
-//   const modalToggle = (photoId) => {
-//       setModal(prev => ({...prev, display: !prev.display, id: photoId}))
-//   };
-  
-const {
-  state,
-  onPhotoSelect,
-  updateToFavPhotoIds,
-  onClosePhotoDetailsModal,
-} = useApplicationData();
+  const {
+    state,
+    onPhotoSelect,
+    updateToFavPhotoIds,
+    onClosePhotoDetailsModal,
+  } = useApplicationData();
 
   return (
     <div>
-      <HomeRoute photos={photos} topics={topics} modalToggle={onPhotoSelect} fav={state.fav} clickHandler={updateToFavPhotoIds}/>
-      {state.modal.display && <PhotoDetailsModal closeModal={onClosePhotoDetailsModal} photos={photos} fav={state.fav} clickHandler={updateToFavPhotoIds} showModal={state.modal} />}
+      <HomeRoute
+        photos={photos}
+        topics={topics}
+        modalToggle={onPhotoSelect}
+        fav={state.fav}
+        clickHandler={updateToFavPhotoIds}
+      />
+      {state.modal.display && (
+        <PhotoDetailsModal
+          closeModal={onClosePhotoDetailsModal}
+          photos={photos}
+          fav={state.fav}
+          clickHandler={updateToFavPhotoIds}
+          showModal={state.modal}
+        />
+      )}
     </div>
   );
 };
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,22 +1,22 @@
-import { useState} from "react";
+import { useState } from "react";
 
-export const useApplicationData = function() {
-  
-  const [fav , setFav] = useState({});
-  
+export const useApplicationData = function () {
+
+  const [fav, setFav] = useState({});
+
   const clickHandler = (photoId) => {
     setFav(prev => (
-      {...prev, [photoId]: !prev[photoId]}
+      { ...prev, [photoId]: !prev[photoId] }
     ))
   };
-  
- const [showModal, setModal] = useState({display: false, id: ""});
-  
-  
+
+  const [showModal, setModal] = useState({ display: false, id: "" });
+
+
   const modalToggle = (photoId) => {
-      setModal(prev => ({...prev, display: !prev.display, id: photoId}))
+    setModal(prev => ({ ...prev, display: !prev.display, id: photoId }))
   };
-  const state = {modal: showModal, fav: fav};
+  const state = { modal: showModal, fav: fav };
 
   const onPhotoSelect = modalToggle;
 
@@ -28,14 +28,8 @@ export const useApplicationData = function() {
     state,
     onPhotoSelect,
     updateToFavPhotoIds,
-    onClosePhotoDetailsModal,};
+    onClosePhotoDetailsModal,
+  };
 
-  return ( appData )
+  return (appData)
 };
-
-// export default useApplicationData;
-
-// The state object will contain the entire state of the application.
-// The updateToFavPhotoIds action can be used to set the favourite photos.
-// The setPhotoSelected action can be used when the user selects a photo.
-// The onClosePhotoDetailsModal action can be used to close the modal.
